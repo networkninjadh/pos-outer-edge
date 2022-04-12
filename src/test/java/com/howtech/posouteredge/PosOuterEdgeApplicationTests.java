2d261afe06dc383ebe1a7166e48a9ab2f7a639c1
networkninjadh <networkninjadh@gmail.com>
2022-04-12 12:19:19 -0500

    initital commit

---
+++ src/test/java/com/howtech/posouteredge/PosOuterEdgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howtech.posouteredge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosOuterEdgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
